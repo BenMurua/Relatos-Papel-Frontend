70af24190c18d748a86052a8cfe4c1b274851b44
Adrian <ortizdev@proton.me>
2025-05-13 18:44:42 +0200

    adding dark/Light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      {/* 
-      TODO : REVISAR CON EL EQUIPO, NO ES BUENA PRACTICA ARMAR EL LAYOUT EN EL APP.JSX
-      DEBERIA SER UN COMPONENTE QUE SE LLAME LAYOUT Y QUE este componente contenga la estructura de nuestra app
-      Y QUE SE USE EN EL ROUTER;
-    */}
       <Outlet />
     </>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,46 @@
 import "./Header.css";
+import { useContext } from "react";
 
-import logo from "../../assets/LogoWhite.svg";
-import cart from "../../assets/CartWhite.svg";
+// Logos y carritos según el tema
+import logoLight from "../../assets/LogoWhite.svg"; // Logo para modo claro
+import logoDark from "../../assets/LogoBlack.svg"; // Logo para modo oscuro
+import cartLight from "../../assets/CartWhite.svg"; // Carrito para modo claro
+import cartDark from "../../assets/CartBlack.svg"; // Carrito para modo oscuro
+
+import { ThemeContext } from "../../context/themeContext";
 
 const Header = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  // Elegir el logo y el carrito según el tema
+  const logoSrc = theme === "light" ? logoDark : logoLight;
+  const cartSrc = theme === "light" ? cartDark : cartLight;
+
   return (
-    <header className="header_bar">
-      <div className="logo_nombre">
-        <img className="logo" src={logo} />
-        <span>Relatos de papel</span>
+    <header
+      className={`header_bar ${theme === "dark" ? "dark-mode" : "light-mode"}`}
+    >
+      <div className="logo-container">
+        <img className="logo" src={logoSrc} alt="Logo" /> {/* Logo dinámico */}
+        <span className="logo-text">Relatos de papel</span>
       </div>
-      <img className="cart" src={cart} />
+
+      <nav className="nav-links">
+        <button
+          onClick={toggleTheme}
+          className="theme-toggle"
+          aria-label={`Cambiar a ${
+            theme === "light" ? "modo oscuro" : "modo claro"
+          }`}
+        >
+          <span className="material-symbols-outlined">contrast</span>
+        </button>
+      </nav>
+
+      <div className="cart-container">
+        <img className="cart" src={cartSrc} alt="Cart" />{" "}
+        {/* Carrito dinámico */}
+      </div>
     </header>
   );
 };
