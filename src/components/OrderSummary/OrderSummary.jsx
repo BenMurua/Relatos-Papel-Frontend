1d2d7bcfdbcd0661f2d846e712e58a4daad908a3
alberto2606 <albertolaracano@gmail.com>
2025-05-13 21:26:53 +0200

    Checkout improvements

--- src/components/OrderSummary/OrderSummary.jsx
+++ src/components/OrderSummary/OrderSummary.jsx
@@ -1,45 +1,50 @@
 import "./OrderSummary.css";
-/* Los valores no pueden estar hardcodeados, deberán recibirse mediante props de manera dinámica */
 
-const OrderSummary = () => {
+const OrderSummary = ({ title, products }) => {
+  const subtotal = products.reduce(
+    (sum, item) => sum + item.quantity * item.price,
+    0
+  );
+  const shippingCost = 5;
+  const total = subtotal + shippingCost;
+
   return (
     <div className="order-summary">
-      <h2 className="order-summary__title">TU PEDIDO</h2>
+      <h2 className="order-summary__title">{title}</h2>
       <br />
 
       <div className="order-summary__product-list">
         <h2 className="order-summary__subtitle">PRODUCTO</h2>
         <br />
 
-        <div className="order-summary__row">
-          <div className="order-summary__item-name">El Principito</div>
-          <div className="order-summary__item-price">x1 20€</div>
-        </div>
-
-        <div className="order-summary__row">
-          <div className="order-summary__item-name">El lobo estepario</div>
-          <div className="order-summary__item-price">x1 20€</div>
-        </div>
+        {products.map((product, index) => (
+          <div key={index} className="order-summary__row">
+            <div className="order-summary__item-name">{product.name}</div>
+            <div className="order-summary__item-price">
+              x{product.quantity} {product.price}€
+            </div>
+          </div>
+        ))}
 
         <div className="order-summary__row">
           <div className="order-summary__item-name">
             <h3 className="order-summary__label">Subtotal</h3>
           </div>
-          <div className="order-summary__item-price">40€</div>
+          <div className="order-summary__item-price">{subtotal}€</div>
         </div>
 
         <div className="order-summary__row">
           <div className="order-summary__item-name">
             <h3 className="order-summary__label">Envío</h3>
           </div>
-          <div className="order-summary__item-price">5€</div>
+          <div className="order-summary__item-price">{shippingCost}€</div>
         </div>
 
         <div className="order-summary__row order-summary__row--total">
           <div className="order-summary__item-name">
             <h2 className="order-summary__total-label">TOTAL</h2>
           </div>
-          <div className="order-summary__item-price">45€</div>
+          <div className="order-summary__item-price">{total}€</div>
         </div>
       </div>
     </div>
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -7,13 +7,16 @@ const Checkout = () => {
   return (
     <div className="checkout">
       <div className="checkout__left">
+        <h1 className="checkout__title">Datos de Facturación</h1>
         <FacturationUserForm />
       </div>
       <div className="checkout__right">
         <div className="checkout__summary">
+          <h2 className="checkout__subtitle">Resumen del pedido</h2>
           <OrderSummary />
         </div>
         <div className="checkout__payment">
+          <h2 className="checkout__subtitle">Método de pago</h2>
           <PaymentMethod />
         </div>
       </div>
