22384a6a207e0ceb57ac6309592a97474a4865df
Adrian <ortizdev@proton.me>
2025-05-13 20:56:24 +0200

    changes requested

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,17 +8,20 @@ import cartLight from "../../assets/CartWhite.svg"; // Carrito para modo claro
 import cartDark from "../../assets/CartBlack.svg"; // Carrito para modo oscuro
 
 import { ThemeContext } from "../../context/themeContext";
+import { THEMES } from "../../models/constants";
 
 const Header = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   // Elegir el logo y el carrito según el tema
-  const logoSrc = theme === "light" ? logoDark : logoLight;
-  const cartSrc = theme === "light" ? cartDark : cartLight;
+  const logoSrc = theme === THEMES.LIGHT ? logoDark : logoLight;
+  const cartSrc = theme === THEMES.LIGHT ? cartDark : cartLight;
 
   return (
     <header
-      className={`header_bar ${theme === "dark" ? "dark-mode" : "light-mode"}`}
+      className={`header_bar ${
+        theme === THEMES.DARK ? "dark-mode" : "light-mode"
+      }`}
     >
       <div className="logo-container">
         <img className="logo" src={logoSrc} alt="Logo" /> {/* Logo dinámico */}
@@ -30,7 +33,7 @@ const Header = () => {
           onClick={toggleTheme}
           className="theme-toggle"
           aria-label={`Cambiar a ${
-            theme === "light" ? "modo oscuro" : "modo claro"
+            theme === THEMES.LIGHT ? "modo oscuro" : "modo claro"
           }`}
         >
           <span className="material-symbols-outlined">contrast</span>
@@ -39,7 +42,6 @@ const Header = () => {
 
       <div className="cart-container">
         <img className="cart" src={cartSrc} alt="Cart" />{" "}
-        {/* Carrito dinámico */}
       </div>
     </header>
   );
--- src/context/themeContext.jsx
+++ src/context/themeContext.jsx
@@ -1,10 +1,11 @@
 import { createContext, useState, useEffect } from "react";
+import { THEMES } from "../models/constants";
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(
-    localStorage.getItem("app-theme") || "light" // Recuperar tema del localStorage
+    localStorage.getItem("app-theme") || THEMES.LIGHT // Recuperar tema del localStorage
   );
 
   useEffect(() => {
@@ -13,7 +14,9 @@ export const ThemeProvider = ({ children }) => {
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    setTheme((prevTheme) =>
+      prevTheme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT
+    );
   };
 
   return (
