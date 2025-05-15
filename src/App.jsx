d14de200d3ac1971c63ef975970f616b6898efd1
Enrique Lovera <lovera@MacBook-Air-de-Enrique.local>
2025-05-15 19:03:51 +0200

    delete unused context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import { ThemeProvider } from "./context/themeContext";
-import { BookCardProvider } from "./context/bookCardContext";
 
 function App() {
-  return (
-    <ThemeProvider>
-      <BookCardProvider>
-        <Outlet />
-      </BookCardProvider>
-    </ThemeProvider>
-  );
+  return <Outlet />;
 }
 
 export default App;
